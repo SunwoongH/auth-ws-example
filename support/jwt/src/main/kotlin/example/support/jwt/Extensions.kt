357805587562ac63cb91e0ced8cec81c4e7352bb
sunwoong <sunwoong1122@gmail.com>
2024-07-04 21:54:17 +0900

    feat: jwt 유틸 extension 구현

---
+++ support/jwt/src/main/kotlin/example/support/jwt/Extensions.kt
@@ -0,0 +1,10 @@
+package example.support.jwt
+
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.Date
+
+fun LocalDateTime.toDate(): Date {
+    val instant = this.atZone(ZoneId.systemDefault()).toInstant()
+    return Date.from(instant)
+}
